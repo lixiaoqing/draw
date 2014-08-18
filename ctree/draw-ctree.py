d88b38d5a1775cc454360d551d86652ec90360de
xqli <lix0715@gmail.com>
2014-08-18 20:32:37 +0800

    add figure-type when draw ctree

--- ctree/draw-ctree.py
+++ ctree/draw-ctree.py
@@ -2,8 +2,9 @@
 import sys
 from nltk.tree import Tree
 
-parse_file = sys.argv[1]
-line_num = int(sys.argv[2])
+flag = sys.argv[1]
+parse_file = sys.argv[2]
+line_num = int(sys.argv[3])
 fout = open('tree.tex','w')
 print >>fout,r'''\documentclass[tikz]{standalone}
 \usepackage{CJKutf8}
@@ -20,10 +21,13 @@ for i,s in enumerate(f):
     if i == line_num:
         s = s.replace('$','\$')
         tree = Tree.parse(s)
-        h = tree.height()
-        print >>fout,'''\\begin{{scope}}[frontier/.style={{distance from root={}}}]\n'''.format(h*28)
-        for pos in tree.treepositions('leaves'):
-	    tree[pos] = r'\edge[dashed]; {' + tree[pos] + '}'
+        if flag == '0':
+            h = tree.height()
+            print >>fout,'''\\begin{{scope}}[frontier/.style={{distance from root={}}}]\n'''.format(h*28)
+            for pos in tree.treepositions('leaves'):
+	        tree[pos] = r'\edge[dashed]; {' + tree[pos] + '}'
+        else:
+            print >>fout,r'\begin{scope}'
         print >>fout,tree.pprint_latex_qtree()
         break
 print >>fout,r'''
--- ctree/draw-ctree.sh
+++ ctree/draw-ctree.sh
@@ -1,8 +1,8 @@
 if [ $# = 0 ];then
-	echo "usage: draw-tree.sh parse-file line-num"
+	echo "usage: draw-tree.sh figure-type(0/1) parse-file line-num"
 	exit 0
 fi
 set -x
-python /home/xqli/tools/draw/ctree/draw-ctree.py $1 $2
+python /home/xqli/tools/draw/ctree/draw-ctree.py $1 $2 $3
 pdflatex tree.tex > /dev/null
 rm tree.tex tree.aux tree.log
