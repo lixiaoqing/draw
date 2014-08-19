05d70d9cf4aca2f2211f102465230a3cbb5f6115
xqli <lix0715@gmail.com>
2014-08-19 09:28:29 +0800

    draw two types of dependency tree

--- dtree/draw-dtree.py
+++ dtree/draw-dtree.py
@@ -1,51 +1,86 @@
-#!/usr/bin/python
-#encoding=utf8
 import sys
 from collections import defaultdict
+from nltk.parse import DependencyGraph
 
-fn = sys.argv[1]
-line_num = int(sys.argv[2])
-
+flag = sys.argv[1]
+parse_file = sys.argv[2]
+line_num = int(sys.argv[3])
 fout = open('tree.tex','w')
 print >>fout,r'''\documentclass[tikz]{standalone}
 \usepackage{CJKutf8}
 \usepackage{color}
 \usepackage{tikz}
 \usepackage{tikz-dependency}
+\usepackage{tikz-qtree}
 \thispagestyle{empty}
 \begin{document}
 \begin{CJK}{UTF8}{gbsn}
+'''
 
-\begin{dependency}[theme = simple]
+if flag == '0':
+    print >>fout,r'''\begin{dependency}[theme = simple]
 \begin{deptext}[column sep=1em] '''
-head2children = defaultdict(list)
-sen_src = []
-i = -1
-for s in open(fn):
-    if len(s.strip()) == 0:
-        i += 1
-        if i == line_num:
-            print >>fout,' \& '.join(sen_src) + r' \\'
-            print >>fout,r'\end{deptext}'
-            print >>fout,r'\deproot{'+str(head2children[0][0])+'}{}'
-            del head2children[0]
-            for head,deps in head2children.items():
-                for dep in deps:
-                    print >>fout,r'\depedge{'+str(head)+'}{'+str(dep)+'}{}'
-            break
-        head2children = defaultdict(list)
-        sen_src = []
-    else:
-        s = s.split()
-        dep = int(s[0])
-        head = int(s[6])
-        word = s[1]
-        head2children[head].append(dep)
-        sen_src.append(word)
-
-print >>fout,r'''
+    head2children = defaultdict(list)
+    sen_src = []
+    i = -1
+    for s in open(parse_file):
+        if len(s.strip()) == 0:
+            i += 1
+            if i == line_num:
+                print >>fout,' \& '.join(sen_src) + r' \\'
+                print >>fout,r'\end{deptext}'
+                print >>fout,r'\deproot{'+str(head2children[0][0])+'}{}'
+                del head2children[0]
+                for head,deps in head2children.items():
+                    for dep in deps:
+                        print >>fout,r'\depedge{'+str(head)+'}{'+str(dep)+'}{}'
+                break
+            head2children = defaultdict(list)
+            sen_src = []
+        else:
+            s = s.split()
+            dep = int(s[0])
+            head = int(s[6])
+            word = s[1]
+            head2children[head].append(dep)
+            sen_src.append(word)
+    
+    print >>fout,r'''
 \end{dependency}
 \end{CJK}
 \end{document}
 '''
-
+else:
+    print >>fout,r'''\begin{tikzpicture}'''
+    i = -1
+    dep_str = ''
+    for s in open(parse_file):
+        if len(s.strip()) == 0:
+            i += 1
+            if i == line_num:
+                dg = DependencyGraph(dep_str)
+                tree = dg.tree()
+                if flag == '0':
+                    h = tree.height()
+                    #print >>fout,'''\\begin{{scope}}[frontier/.style={{distance from root={}}}]\n'''.format(h*28)
+                    print >>fout,'''\\begin{{scope}}\n'''.format(h*28)
+                    for pos in tree.treepositions('leaves'):
+    	                tree[pos] = r'\edge[dashed]; {' + tree[pos] + '}'
+                else:
+                    print >>fout,r'\begin{scope}'
+                print >>fout,tree.pprint_latex_qtree()
+                break
+            dep_str = ''
+        else:
+            s = s.split()
+            self,word,pos,head,rel = int(s[0]),s[1],s[3],int(s[6]),s[7]
+            dep_str += ' '.join([word,pos,str(head),rel])+'\n'
+            #dep_str += ' '.join([word,pos,str(max(2*head-1,0)),rel])+'\n'
+            #dep_str += ' '.join([word,pos,str(2*self-1),'MOD'])+'\n'
+    
+    print >>fout,r'''
+\end{scope}
+\end{tikzpicture}
+\end{CJK}
+\end{document}
+'''
--- dtree/draw-dtree.sh
+++ dtree/draw-dtree.sh
@@ -1,8 +1,8 @@
 if [ $# = 0 ];then
-	echo "usage: draw-dtree.sh dep-file line-num"
+	echo "usage: draw-dtree.sh figure-type dep-file line-num"
 	exit 0
 fi
 set -x
-python /home/xqli/tools/draw/dtree/draw-dtree.py $1 $2
+python /home/xqli/tools/draw/dtree/draw-dtree.py $1 $2 $3
 pdflatex tree.tex > /dev/null
 rm tree.tex tree.aux tree.log
